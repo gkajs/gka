b8037f0d7d56293157907f87f1a52c236b64fe55
joeyguo <joeyguo@tencent.com>
2017-03-30 15:42:01 +0800

    add test

--- test/gka.test.js
+++ test/gka.test.js
@@ -1,83 +1,93 @@
 var gka = require("../lib/gka");
 var fs = require('fs');
 var path = require('path');
+var md5 = require('md5');
+var assert = require('assert');
 
+var imgFolder = path.join(__dirname, "img4test");
 var expectedDir = path.join(__dirname, 'expected');
 var expectedDir_normal = path.join(expectedDir, 'img4test-gka');
 var expectedDir_sprites = path.join(expectedDir, 'img4test-gka-sprites');
 
-var imgFolder = path.join(__dirname, "img4test");
+var targetDir_normal = path.join(__dirname, 'img4test-gka');
+var targetDir_sprites = path.join(__dirname, 'img4test-gka-sprites');
 
-// TODO 
-/*
-describe('gka-actual-test', function () {
+describe('gka actual test', function () {
 
-    it('gka-normal：node gka.js -d dir', function (next) {
-        // 生成帧动画 - 普通模式
+     before(function runFn (done) {
+        // gka - normal model
         gka({
-            dir: d,
-            prefix: p,
+            dir: imgFolder,
+            prefix: "gka-",
             type: "normal"
         });
 
-        setTimeout(()=>{
-
-            next();
-        }, 10000)
-    });
-
-    it('gka-sprites：gka -d dir -s true', function (next) {
-        // 生成帧动画 - 普通模式
+        // gka - sprites model
         gka({
-            dir: d,
-            prefix: p,
+            dir: imgFolder,
+            prefix: "gka-",
             type: "sprites"
         });
 
         setTimeout(()=>{
+            done();
+        }, 12000);
+    });
 
-            next();
-        }, 10000)
+    after(function cleanup () {
+        deleteall(targetDir_normal);
+        deleteall(targetDir_sprites);
+    });
+
+    it('gka-normal：gka -d dir -p gka', function () {
+        assert.deepEqual(getDirFile2Md5(expectedDir_normal), getDirFile2Md5(targetDir_normal), 'expect the same');
+    });
+
+    it('gka-sprites：gka -d dir -p gka -s true', function () {
+        assert.deepEqual(getDirFile2Md5(expectedDir_sprites), getDirFile2Md5(targetDir_sprites), 'expect the same');
     });
 
 });
-*/
 
-// var tiny = require("./core/tiny");
 
-/*
-var program = {
-    tiny: false,
-    sprites: true,
-    dir: "E:/gka-test/img",
-    prefix: "prefix22",
 
-};
+function getFiles (dir, _files){
+    _files = _files || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = path.join(dir, files[i]);
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, _files);
+        } else {
+            _files.push(name);
+        }
+    }
+    return _files;
+}
 
-var t = program.tiny,
-    s = program.sprites,
-    d = program.dir,
-    p = program.prefix;
+function deleteall(path) {  
+    var files = [];  
+    if(fs.existsSync(path)) {  
+        files = fs.readdirSync(path);  
+        files.forEach(function(file, index) {  
+            var curPath = path + "/" + file;  
+            if(fs.statSync(curPath).isDirectory()) {
+                deleteall(curPath);  
+            } else {
+                fs.unlinkSync(curPath);  
+            }  
+        });  
+        fs.rmdirSync(path);  
+    }  
+}
 
-if (t) {
-
-    // 图片压缩
-    tiny(t);
-
-} else if (s) {
-
-    // 生成帧动画 - 合图模式
-    gka({
-        dir: d,
-        prefix: p,
-        type: "sprites"
-    });
-
-} else {
-    // 生成帧动画 - 普通模式
-    gka({
-        dir: d,
-        prefix: p,
-        type: "normal"
-    });
-}*/
+function getDirFile2Md5 (dir) {
+    var filepaths = getFiles(dir);
+    var name2md5 = {};
+    for (var i = 0, data, filepath; i < filepaths.length; i++) {
+        filepath = filepaths[i];
+        data = fs.readFileSync(filepath);
+        name2md5[path.relative(dir, filepaths[i])] = md5(data);
+    }
+    return name2md5;
+}
