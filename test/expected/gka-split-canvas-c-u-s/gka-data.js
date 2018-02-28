d41911621cb8b3ae50c89dbfe2365ccfbe29f10e
joeyguo <lgsincy@gmail.com>
2018-03-01 00:18:03 +0800

    update test

---
+++ test/expected/gka-split-canvas-c-u-s/gka-data.js
@@ -0,0 +1,26 @@
+var data = {
+    "width": 661,
+    "height": 20,
+    "offX": 6,
+    "offY": 1,
+    "sourceW": 670,
+    "sourceH": 23,
+    "w": 1983,
+    "h": 20,
+    "y": 0,
+    "file": "sprites.png",
+    "frames": [
+        {
+            "x": 0
+        },
+        {
+            "x": 661
+        },
+        {
+            "x": 1322
+        }
+    ],
+    "animations": {
+        "": [0,1,2]
+    }
+}
