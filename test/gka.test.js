ecef3bd834c3b672ee60257440041bcd0210ae5d
joeyguo <lgsincy@gmail.com>
2018-05-03 22:21:57 +0800

    refactor

--- test/gka.test.js
+++ test/gka.test.js
@@ -11,48 +11,80 @@ var expectedDir = path.join(__dirname, 'expected');
 var afterDelete = true;
 
 // 测试的方法集合
-var commanderList = [
-    'node ./bin/gka.js ./test/split',
-    'node ./bin/gka.js ./test/split -csu',
-    'node ./bin/gka.js ./test/split -t svg',
-    'node ./bin/gka.js ./test/split -t svg -csu',
-    'node ./bin/gka.js ./test/split -t canvas',
-    'node ./bin/gka.js ./test/split -t canvas -csu',
-    'node ./bin/gka.js ./test/diff -t canvas --diff',
-    'node ./bin/gka.js ./test/split --split -cus -p a -t canvas -f 0.08',
-    'node ./bin/gka.js ./test/retina',
-    'node ./bin/gka.js ./test/mulit',
-    'node ./bin/gka.js ./test/split -t percent -su',
-    'node ./bin/gka.js ./test/split -t studiojs -csu',
-    'node ./bin/gka.js ./test/split -t createjs -csu',
+var commanderMap = [
+    {
+        commander: 'node ./bin/gka.js ./test/split',
+        expected: 'gka-split-css',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -csu',
+        expected: 'gka-split-css-c-u-s',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t svg',
+        expected: 'gka-split-svg',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t svg -csu',
+        expected: 'gka-split-svg-c-u-s',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t canvas',
+        expected: 'gka-split-canvas',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t canvas -csu',
+        expected: 'gka-split-canvas-c-u-s',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/diff -t canvas --diff',
+        expected: 'gka-diff-canvas-d',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split --split -cus -p a -t canvas -f 0.08',
+        expected: 'gka-split-canvas-c-u-s-a-sp',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/retina',
+        expected: 'gka-retina-css',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/mulit',
+        expected: 'gka-mulit-css',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t percent -su',
+        expected: 'gka-split-percent-u-s',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t studiojs -csu',
+        expected: 'gka-split-studiojs-c-u-s',
+    },
+    {
+        commander: 'node ./bin/gka.js ./test/split -t createjs -csu',
+        expected: 'gka-split-createjs-c-u-s',
+    },
 ]
 // -----------
 
-for (var i = 0, commander; i < commanderList.length; i++) {
-    commander = commanderList[i];
-    ((commander) => {
-        describe(commander, function () {
-            var output = '';
-            before(function runFn (done) {
-                var s = execSync(`${commander} --env test`).toString();
-                s.replace(/\[output dir\]: "(.*?)"/,function($0,$1){
-                    output = $1;
-                });
-                output = path.basename(output);
-                setTimeout(()=>{
-                    done();
-                }, 2000);
-            });
-            after(function cleanup () {
-                console.log(output)
-                afterDelete && deleteall(path.join(__dirname, output));
-            });
-            it(commander, function () {
-                assert.deepEqual(getDirFile2Md5(path.join(expectedDir, output)), getDirFile2Md5(path.join(__dirname, output)), 'expect the same');
-            });
+commanderMap.map(({commander, expected}) => {
+    describe(commander, function () {
+        var output = '';
+        before(function runFn (done) {
+            execSync(`${commander} --env test`).toString();
+            setTimeout(()=>{
+                done();
+            }, 2000);
         });
-    })(commander)
-}
+        after(function cleanup () {
+            afterDelete && deleteall(path.join(__dirname, expected));
+        });
+        it(commander, function () {
+            console.log(path.join(expectedDir, expected))
+            assert.deepEqual(getDirFile2Md5(path.join(expectedDir, expected)), getDirFile2Md5(path.join(__dirname, expected)), 'expect the same');
+        });
+    });
+})
 
 function getFiles (dir, _files){
     _files = _files || [];
